d2d558207fb99aaca210deb133ce21156bdca2d7
Demeure Romain <demeureromain4@gmail.com>
2022-07-12 21:46:31 +0200

    modale works

--- src/components/foodtruckcomponents/Modal.vue
+++ src/components/foodtruckcomponents/Modal.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="bloc-modale">
+    <div class="bloc-modale" v-if="revele">
         <div class="overlay"></div>
 
         <div class="modale"></div>
-            <button>fermer</button>
+            <button class="btn" @click="toggleModale">fermer</button>
             <h2>Le contenu de la modale</h2>
         
     </div>
@@ -11,12 +11,19 @@
 
 <script>
 export default {
-    name: 'Modale'
+    name: 'Modale',
+    props: ['revele', 'toggleModale']
+
 }
 </script>
 
 <style scoped>
 .bloc-modale {
     background: lightblue;
+    position: fixed;
+    justify-content: center;
+    align-items: center;
 }
+
+
 </style>
--- src/components/foodtruckcomponents/main.vue
+++ src/components/foodtruckcomponents/main.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="container">
         <h1>Le FoodTruck</h1>
-        <modale></modale>
+        <modale v-bind:revele="revele" v-bind:toggleModale="toggleModale"></modale>
+        <button class="btn" @click="toggleModale">Ouvre la modale</button>
     </div>
 </template>
 
@@ -10,9 +11,19 @@ import Modale from './Modal.vue'
 
 export default {
     name: 'Contenu',
+    data(){
+        return {
+            revele: false
+        }
+    },
     components: {
         'modale': Modale
-    }
+    },
+    methods: {
+        toggleModale: function(){
+            this.revele = !this.revele
+        }
+    },
 }
 
 </script>
